278e9b0d492a8d776965aaaa1f62c9c6bfa6c724
Bazil Korath <korathbasil@live.com>
2022-03-16 22:18:42 +0530

    server: working product controller

--- server/src/product/product.controller.ts
+++ server/src/product/product.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ProductService } from './product.service';
+
+import { Product } from './product.service';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+    constructor(private readonly producttService : ProductService){}
+
+    @Get()
+    getProduct(): Product[] {
+        return this.producttService.getAllProducts()
+    }
+}
--- server/src/product/product.service.ts
+++ server/src/product/product.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
 
+export class Product {
+    id: string
+    name: string
+
+    constructor(_id: string, _name: string) {
+        this.id = _id;
+        this.name = _name;
+    }
+}
+
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    getAllProducts(): Product[] {
+        const products = [
+            new Product("23232", "James")
+        ];
+
+        return products;
+    }
+}
