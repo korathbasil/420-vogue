4310961c7e50e8339c72ef983af2ed16bdb740c5
Bazil Korath <korathbasil@live.com>
2022-03-23 11:54:46 +0530

    client: add icons to header

--- client/components/header/Header.tsx
+++ client/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from "./header.module.scss";
+import { Cart, Avatar } from "../../assets/icons";
 
 export const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.left}>420 vogue</div>
-      <div className={styles.right}>cart</div>
+      <div className={styles.right}>
+        <Cart />
+        <Avatar />
+      </div>
     </header>
   );
 };
