c0ccb75ca8c28b9495d2774eda9aff7209d04f17
Bazil Korath <korathbasil@live.com>
2022-03-29 03:38:51 +0530

    client: add link to product page on product card

--- client/components/productCard/productCard.tsx
+++ client/components/productCard/productCard.tsx
@@ -1,12 +1,16 @@
+import Link from "next/link";
+
 import styles from "./productCard.module.scss";
 
 export const ProductCard = () => {
   return (
-    <div className={styles.card}>
-      <img
-        src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
-        alt=""
-      />
-    </div>
+    <Link href={"/product"}>
+      <div className={styles.card}>
+        <img
+          src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
+          alt=""
+        />
+      </div>
+    </Link>
   );
 };
