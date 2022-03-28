490c4a45b14aad302689664ceda82dd1ff4c1a3a
Bazil Korath <korathbasil@live.com>
2022-03-29 04:43:29 +0530

    client:  add image to product page

---
+++ admin-client/pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+import styles from  '../styles/login.module.scss';
+
+const LoginPage : NextPage = () => {
+    return <section className={styles.loginpage}>
+        
+    </section>
+}
+
+export default LoginPage;
