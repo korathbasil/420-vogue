6eb52e66583c395cf76dbc7fce3d89734eb37cb5
Bazil Korath <korathbasil@live.com>
2022-03-29 03:36:52 +0530

    client: add dummy images for product card

--- client/components/productCard/productCard.tsx
+++ client/components/productCard/productCard.tsx
@@ -1,5 +1,12 @@
 import styles from "./productCard.module.scss";
 
 export const ProductCard = () => {
-  return <div className={styles.card}></div>;
+  return (
+    <div className={styles.card}>
+      <img
+        src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
+        alt=""
+      />
+    </div>
+  );
 };
