0e43b4eb3770294d0c8a76736efbb22260cdc470
Bazil Korath <korathbasil@live.com>
2022-03-29 04:42:20 +0530

    client:  add image to product page

--- client/pages/product/index.tsx
+++ client/pages/product/index.tsx
@@ -1,7 +1,23 @@
 import { NextPage } from "next";
 
+import styles from "./product.module.scss";
+
 const ProductPage: NextPage = () => {
-  return <section>Product</section>;
+  return (
+    <section className={styles.parent}>
+      <div className={styles.header}>Go Back</div>
+      <div className={styles.images}>
+        <img
+          src={
+            "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
+          }
+        />
+      </div>
+      <div className={styles.actions}>
+        <p>Add to Card</p>
+      </div>
+    </section>
+  );
 };
 
 export default ProductPage;
