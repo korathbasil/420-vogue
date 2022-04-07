6c84147dddfc393118e6c568b8f2d35c6ec80c4a
Bazil Korath <korathbasil@live.com>
2022-04-07 12:10:58 +0530

    client: add color to icons, secondary col

--- client/assets/icons/backArrow.tsx
+++ client/assets/icons/backArrow.tsx
@@ -1,5 +1,8 @@
 export const BackArrow = () => (
   <svg viewBox="0 0 24 24">
-    <path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"></path>
+    <path
+      fill="#ff004c"
+      d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"
+    ></path>
   </svg>
 );
--- client/assets/icons/menu.tsx
+++ client/assets/icons/menu.tsx
@@ -1,5 +1,8 @@
 export const Menu = () => (
   <svg viewBox="0 0 24 24">
-    <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
+    <path
+      fill="#ff004c"
+      d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+    ></path>
   </svg>
 );
