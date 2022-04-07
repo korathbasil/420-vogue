9b0edc108b11fe7908fb1de6198cc8e69a02915d
Bazil Korath <korathbasil@live.com>
2022-04-07 23:45:40 +0530

    feat [client]: add product carousel without scroling, yup for now :)

--- client/components/index.ts
+++ client/components/index.ts
@@ -5,3 +5,4 @@ export * from "./productCard/productCard";
 export * from "./recentItems/recentItems";
 export * from "./customHeader/customHeader";
 export * from "./sidebar/sidebar";
+export * from "./productCarousel/productCarousel";
--- client/pages/product/index.tsx
+++ client/pages/product/index.tsx
@@ -1,21 +1,12 @@
 import { NextPage } from "next";
 
 import styles from "./product.module.scss";
+import { ProductCarousel } from "components";
 
 const ProductPage: NextPage = () => {
   return (
     <section className={styles.parent}>
-      <div className={styles.header}>Go Back</div>
-      <div className={styles.images}>
-        <img
-          src={
-            "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
-          }
-        />
-      </div>
-      <div className={styles.actions}>
-        <p>Add to Card</p>
-      </div>
+      <ProductCarousel />
     </section>
   );
 };
