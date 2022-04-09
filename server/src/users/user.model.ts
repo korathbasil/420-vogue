7fc7183980b5ebfe42ca4481b7c85148574ece1f
Bazil Korath <korathbasil@live.com>
2022-04-09 11:40:42 +0530

    feat [server]: add user model props

--- server/src/users/user.model.ts
+++ server/src/users/user.model.ts
@@ -1,7 +1,36 @@
 import { Schema, SchemaFactory, Prop } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+class UserAccount extends Document {
+  @Prop({ type: String, required: true })
+  kind: string;
+
+  @Prop({ type: String, required: true })
+  uid: string;
+}
+
+class UserAddress extends Document {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  line1: string;
+
+  @Prop({ type: String, required: true })
+  line2: string;
+
+  @Prop({ type: String })
+  line3: string;
+
+  @Prop({ type: String, required: true })
+  city: string;
+
+  @Prop({ type: String, required: true })
+  pin: string;
+}
 
 @Schema()
-export class User {
+export class User extends Document {
   @Prop({ type: String, required: true })
   firstName: string;
 
@@ -10,6 +39,18 @@ export class User {
 
   @Prop({ type: String, required: true })
   email: string;
+
+  @Prop({ type: String, required: true })
+  phone: string;
+
+  @Prop({ type: String })
+  password?: string;
+
+  @Prop([{ type: UserAccount, required: true }])
+  accounts: UserAccount[];
+
+  @Prop([{ type: UserAddress, required: true }])
+  addresses: UserAddress[];
 }
 
 export const userSchema = SchemaFactory.createForClass(User);
