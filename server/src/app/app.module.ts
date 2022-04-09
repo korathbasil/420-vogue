be8af88ef406995c127bcf2cbfe94670d04336c8
Bazil Korath <korathbasil@live.com>
2022-04-09 12:26:43 +0530

    fix [server]: add usermodule to app module, add mongooseForFeature in usersmodule

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 
+import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from '../products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/ecommerce'),
+    UsersModule,
     ProductsModule,
   ],
 })
--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { userSchema } from './user.model';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: userSchema }])],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
 })
