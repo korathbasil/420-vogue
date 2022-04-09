bd527bf69740d7ada0ee0fa1086fce4a8c5aafa8
Bazil Korath <korathbasil@live.com>
2022-04-09 09:31:52 +0530

    fix [server]: type for product document

--- server/src/products/products.repository.ts
+++ server/src/products/products.repository.ts
@@ -2,13 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, FilterQuery } from 'mongoose';
 
-import { ProductDoc } from './products.model';
 import { Product } from './products.model';
 
 @Injectable()
 export class ProductsRepository {
   constructor(
-    @InjectModel('Product') private readonly productModel: Model<ProductDoc>,
+    @InjectModel('Product') private readonly productModel: Model<Product>,
   ) {}
 
   async find(productsFilterQuery: FilterQuery<Product>): Promise<Product[]> {
