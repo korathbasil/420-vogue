c5d6ac92bdfa1d33795513a2e673968bfaac5216
Bazil Korath <korathbasil@live.com>
2022-04-09 14:06:13 +0530

    fix [server]: syntax

--- server/src/util/crypto-hash.ts
+++ server/src/util/crypto-hash.ts
@@ -3,14 +3,17 @@ import { promisify } from 'node:util';
 
 const scrypt = promisify(_scrypt);
 
-export async function hashString(password: string) {
+export async function hashString(password: string): Promise<string> {
   const salt = randomBytes(8).toString('hex');
   const hash = (await scrypt(password, salt, 32)) as Buffer;
 
   return salt + '.' + hash.toString('hex');
 }
 
-export async function compareHash(hashedPassword: string, password: string) {
+export async function compareHash(
+  hashedPassword: string,
+  password: string,
+): Promise<boolean> {
   const [salt, hash] = hashedPassword.split('.');
 
   const newHash = (await scrypt(password, salt, 32)) as Buffer;
