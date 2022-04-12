7d895891b6b9e387a5a08aae52e307ba79e89f83
Bazil Korath <korathbasil@live.com>
2022-04-12 23:58:32 +0530

    fix [admin-client]: add ket to map

--- admin-client/components/page-title/page-title.tsx
+++ admin-client/components/page-title/page-title.tsx
@@ -5,7 +5,7 @@ export const PageTitle = ({ routes }: { routes: string[] }) => {
   return (
     <div className={styles.title}>
       {routes.map((route, n) => (
-        <div className={styles.route}>
+        <div key={n} className={styles.route}>
           <h3>{route}</h3>
           {routes.length != n + 1 && (
             <div className={styles.iconWrapper}>
