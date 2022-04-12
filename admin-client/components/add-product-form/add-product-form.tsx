d9cbef4658732e0facb90f7924183a223accc855
Bazil Korath <korathbasil@live.com>
2022-04-12 23:51:21 +0530

    feat [admin-client]: add two input in same line form

--- admin-client/components/add-product-form/add-product-form.tsx
+++ admin-client/components/add-product-form/add-product-form.tsx
@@ -1,12 +1,43 @@
-import { ProductPreview } from "components/product-preview/product-preview";
 import styles from "./add-product-form.module.scss";
+import { ProductPreview } from "components/product-preview/product-preview";
 
 export const AddProductForm = () => {
   return (
     <section className={styles.addProduct}>
-      <form>HEll</form>
+      <form>
+        <h4>Please fill the form below.</h4>
+        <FormInput label="Barand Name" name="brandName" />
+        <FormInput label="Style Name" name="styleName" />
+
+        <h5>Add base variant details.</h5>
+        <div className={styles.compoundInput}>
+          <div>
+            <FormInput label="Color" name="color" />
+          </div>
+          <div>
+            <FormInput label="Color Code" name="colorCode" />
+          </div>
+        </div>
+      </form>
       <div className="spacer-Y"></div>
       <ProductPreview />
     </section>
   );
 };
+
+function FormInput({
+  label,
+  type = "text",
+  name,
+}: {
+  label: string;
+  type?: string;
+  name: string;
+}) {
+  return (
+    <div className={styles.input}>
+      <label htmlFor={name}>{label}</label>
+      <input type={type} name={name} />
+    </div>
+  );
+}
