e49ff4a3337bf3fcc265498d5b332c2d578cb02c
Bazil Korath <korathbasil@live.com>
2022-04-16 19:22:08 +0530

    feat [server]: add admin module, createAdminDto

---
+++ server/src/admin/dtos/create-admin.dto.ts
@@ -0,0 +1,24 @@
+import { Length, IsNotEmpty, IsNumberString } from 'class-validator';
+
+export class CreateAdminDto {
+  @Length(3, 25)
+  @IsNotEmpty({ message: "First Name can't be empty" })
+  firstName: string;
+
+  @Length(3, 25)
+  @IsNotEmpty({ message: "Last Name can't be empty" })
+  lastName: string;
+
+  @Length(3, 40)
+  @IsNotEmpty({ message: 'Please provide a valid email' })
+  email: string;
+
+  @Length(10, 10)
+  @IsNumberString({ message: 'Please provide a valid phone number' })
+  @IsNotEmpty({ message: 'Please provide a valid phone number' })
+  phone: string;
+
+  @Length(8, 16)
+  @IsNotEmpty({ message: "Password can't be empty" })
+  password: string;
+}
