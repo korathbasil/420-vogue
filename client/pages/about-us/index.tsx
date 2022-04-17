e2e0db5ed414eff8caca65f78103c492b7b7ad7d
Bazil Korath <korathbasil@live.com>
2022-04-17 20:24:33 +0530

    feat [client]: add orders, help, profile, about us dummy page

---
+++ client/pages/about-us/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const AboutUsPage: NextPage = () => {
+  return <h1>About Us</h1>;
+};
+
+export default AboutUsPage;
---
+++ client/pages/help/index.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const HelpPage: NextPage = () => {
+  return <h1>Help</h1>;
+};
+
+export default HelpPage;
