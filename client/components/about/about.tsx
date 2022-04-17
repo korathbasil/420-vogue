7bd71eee3b0755b4eb674a66ecfe2d981d642e4c
Bazil Korath <korathbasil@live.com>
2022-04-17 21:41:11 +0530

    feat [client]: about page with lorem50

--- client/components/about/about.tsx
+++ client/components/about/about.tsx
@@ -4,6 +4,14 @@ export const About = () => {
   return (
     <div className={styles.about}>
       <h1>420VOGUE</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus amet,
+        assumenda ducimus aspernatur, atque a vitae quas sit aliquam, quia sequi
+        tenetur dolor excepturi praesentium porro vel suscipit vero culpa.
+        Quibusdam dolorem ad fugit sed neque aliquid eligendi earum rem
+        reiciendis eum cum totam ullam, molestiae delectus error. Pariatur,
+        perspiciatis!
+      </p>
     </div>
   );
 };
