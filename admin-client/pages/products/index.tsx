885d92ae240b54022cbc7e95ab67baffe34a29c6
Bazil Korath <korathbasil@live.com>
2022-04-21 05:26:20 +0530

    feat [admin-client]: add dummy data for products table

--- admin-client/pages/products/index.tsx
+++ admin-client/pages/products/index.tsx
@@ -2,15 +2,43 @@ import { NextPage } from "next";
 
 import { CustomTable } from "components";
 import Link from "next/link";
+import { stringify } from "querystring";
 
 const ProductsPage: NextPage = () => {
+  const columns = [
+    {
+      name: "Id",
+      prop: "id",
+    },
+    {
+      name: "Style Name",
+      prop: "styleName",
+    },
+    {
+      name: "Brand Name",
+      prop: "brandName",
+    },
+    {
+      name: "Price",
+      prop: "price",
+    },
+  ];
+
+  const data = [
+    {
+      id: "123",
+      styleName: "Jordan Air",
+      brandName: "Nike",
+      price: "199",
+    },
+  ];
   return (
     <section>
       <h2>Products</h2>
       <Link href={"/products/add"}>
         <a>Add product</a>
       </Link>
-      <CustomTable />
+      <CustomTable columns={columns} data={data} />
     </section>
   );
 };
