a4a408d4574fba04381823534cad1e70d235beb5
Bazil Korath <korathbasil@live.com>
2022-06-02 14:49:18 +0530

    feat: extracted LogoText into seperate comp, and put it on header

--- admin-client/src/components/index.ts
+++ admin-client/src/components/index.ts
@@ -1,3 +1,4 @@
+export * from "./logo-text/logo-text";
 export * from "./header/header";
 export * from "./sidebar/sidebar";
 export * from "./custom-table/custom-table";
---
+++ admin-client/src/components/logo-text/logo-text.tsx
@@ -0,0 +1,10 @@
+export const LogoText = () => (
+  <h1
+    style={{
+      color: "var(--col-primary)",
+      fontFamily: "'Teko', sans-serif",
+    }}
+  >
+    420VOGUE<span style={{ color: "grey" }}>.</span>
+  </h1>
+);
