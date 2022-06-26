d78e03463700785e464b30360f42cf663ce43d76
Bazil Korath <korathbasil@live.com>
2022-06-26 19:40:50 +0530

    feat [admin-client]: add categories file

---
+++ admin-client/src/sys/categories.ts
@@ -0,0 +1,88 @@
+export const categories = [
+  {
+    name: "Footwear",
+    subCategories: [
+      {
+        name: "Shoes",
+      },
+      {
+        name: "Boots",
+      },
+      {
+        name: "Loafers",
+      },
+      {
+        name: "Sandals",
+      },
+      {
+        name: "Slides",
+      },
+    ],
+  },
+  {
+    name: "Topwear",
+    subCategories: [
+      {
+        name: "T-Shirts",
+      },
+      {
+        name: "Casual Shirts",
+      },
+      {
+        name: "Formal Shirts",
+      },
+      {
+        name: "Jackets",
+      },
+      {
+        name: "Sweatshirts",
+      },
+    ],
+  },
+  {
+    name: "Bottomwear",
+    subCategories: [
+      {
+        name: "Jeans",
+      },
+      {
+        name: "Casual Trousers",
+      },
+      {
+        name: "Formal Trousers",
+      },
+      {
+        name: "Shorts",
+      },
+    ],
+  },
+  {
+    name: "Wearables",
+    subCategories: [
+      {
+        name: "Watches",
+      },
+      {
+        name: "Sunglasses",
+      },
+      {
+        name: "Smartwatches",
+      },
+      {
+        name: "Smartbands",
+      },
+      {
+        name: "Headphones",
+      },
+      {
+        name: "Wireless Headphones",
+      },
+      {
+        name: "Speakers",
+      },
+      {
+        name: "Wireless Speakers",
+      },
+    ],
+  },
+];
