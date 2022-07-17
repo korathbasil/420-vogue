dc4e8274f59aec96688b458502052ec7cddb3378
Bazil Korath <korathbasil@live.com>
2022-07-17 19:26:00 +0530

    feat [admin-server]: add seperate project for  admin server, add admin, users module

---
+++ admin-server/src/users/users.controller.ts
@@ -0,0 +1,32 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  BadRequestException,
+} from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dtos/createUser.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getAllUsers() {
+    return this.usersService.findAllUsers();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async createUser(@Body() userData: CreateUserDto) {
+    const user = await this.usersService.createUser(userData);
+
+    if (!user) return new BadRequestException('User already exists');
+
+    return user;
+  }
+}
