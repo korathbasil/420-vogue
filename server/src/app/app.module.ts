ae5113a1fc7a5590d3a9e588f530681bdd16eda5
Bazil Korath <korathbasil@live.com>
2022-07-25 01:41:25 +0530

    chore [server]: remove admin module

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from '../products/products.module';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AdminModule } from 'src/admin/admin.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/ecommerce'),
     UsersModule,
     ProductsModule,
-    AdminModule,
   ],
 })
 export class AppModule {}
