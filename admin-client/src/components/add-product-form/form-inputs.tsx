10c82acee4655722d08ceffc12b05422e5fec6e1
Bazil Korath <korathbasil@live.com>
2022-07-24 05:35:21 +0530

    feat [admin-client]: add formik to add product form

--- admin-client/src/components/add-product-form/form-inputs.tsx
+++ admin-client/src/components/add-product-form/form-inputs.tsx
@@ -8,15 +8,27 @@ export function FormInput({
   label,
   type = "text",
   name,
+  onChange,
+  onBlur,
+  value,
 }: {
   label: string;
   type?: string;
   name: string;
+  onChange?: any;
+  onBlur?: any;
+  value?: any;
 }) {
   return (
     <div className={styles.input}>
       <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} />
+      <input
+        type={type}
+        name={name}
+        onChange={onChange}
+        onBlur={onBlur}
+        value={value}
+      />
     </div>
   );
 }
