23eb987e294fc95579a116115f95a35e7f399ce4
Bazil Korath <korathbasil@live.com>
2022-07-24 20:02:23 +0530

    fix [common-server]: export

--- common-server/src/index.ts
+++ common-server/src/index.ts
@@ -1,2 +1,6 @@
+// Users
 export { UsersModule } from "./users/users.module";
+export { UsersService } from "./users/users.service";
+
 export { ProductsModule } from "./products/products.module";
+export { ProductsService } from "./products/products.service";
