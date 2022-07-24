f85456950bcd2bd7b315607e0ddf34973c384b81
Bazil Korath <korathbasil@live.com>
2022-07-25 01:43:00 +0530

    chore [server]: cleanup

--- server/src/products/products.module.ts
+++ server/src/products/products.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
-import { productSchema } from './products.model';
 import { ProductsController } from './products.controller';
-import { ProductsRepository } from './products.repository';
-import { ProductsService } from './products.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Product', schema: productSchema }]),
   ],
   controllers: [ProductsController],
-  providers: [ProductsService, ProductsRepository],
+  providers: [],
 })
 export class ProductsModule {}
