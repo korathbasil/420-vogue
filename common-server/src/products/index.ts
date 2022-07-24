c5513b4b8c9b09ba3be1766a812bcacbf311c945
Bazil Korath <korathbasil@live.com>
2022-07-25 02:00:17 +0530

    feat [common-server]: change product schema

--- common-server/src/products/index.ts
+++ common-server/src/products/index.ts
@@ -1,4 +1,5 @@
 export { ProductsModule } from "./products.module";
 export { ProductsService } from "./products.service";
+export { Product } from "./products.model";
 
 export { CreateProductDto } from "./dtos/createProduct.dto";
--- common-server/src/products/products.model.ts
+++ common-server/src/products/products.model.ts
@@ -1,17 +1,17 @@
-import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
 
-class ProductSize extends Document {
+class ProductStock extends Document {
   @Prop({ type: String, required: true })
-  unit: string;
+  sizeName: string;
 
-  @Prop([{ type: Number }])
-  sizes: number[];
+  @Prop([{ type: Number, required: true }])
+  quantity: number;
 }
 
 class ProductVariant extends Document {
   @Prop({ type: String, required: true })
-  col: string;
+  color: string;
 
   @Prop({ type: String, required: true })
   colorCode: string;
@@ -19,11 +19,11 @@ class ProductVariant extends Document {
   @Prop([{ type: String, required: true }])
   images: string[];
 
-  @Prop({ type: ProductSize, required: true })
-  size: ProductSize;
-
   @Prop({ type: Number, required: true })
   price: number;
+
+  @Prop({ type: ProductStock, required: true })
+  stock: ProductStock;
 }
 
 @Schema()
@@ -36,6 +36,12 @@ export class Product extends Document {
 
   @Prop({ type: String, required: true })
   styleName: string;
+
+  @Prop({ type: String, required: true })
+  category: string;
+
+  @Prop({ type: String, required: true })
+  subCategory: string;
 
   @Prop([{ type: ProductVariant }])
   variants: ProductVariant[];
