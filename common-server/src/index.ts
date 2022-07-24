015a61a1df3002bd67460bb371cdc77a30b680ce
Bazil Korath <korathbasil@live.com>
2022-07-25 01:38:29 +0530

    feat [common-server]: add validation product creation

--- common-server/src/index.ts
+++ common-server/src/index.ts
@@ -4,5 +4,4 @@ export { UsersService } from "./users/users.service";
 export { UsersRepository } from "./users/users.repository";
 export { Role } from "./users/user.model";
 
-export { ProductsModule } from "./products/products.module";
-export { ProductsService } from "./products/products.service";
+export * from "./products";
---
+++ common-server/src/products/index.ts
@@ -0,0 +1,4 @@
+export { ProductsModule } from "./products.module";
+export { ProductsService } from "./products.service";
+
+export { CreateProductDto } from "./dtos/createProduct.dto";
