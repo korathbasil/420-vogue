632957c490c21b62d0a155ed606e8fa5874a80cf
Bazil Korath <korathbasil@live.com>
2022-08-06 15:00:00 +0530

    fix [admin-client]: change port number

--- admin-client/src/utils/axios.ts
+++ admin-client/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.SERVER_ADDRESS || "http://localhost:8000";
+const BASE_URL = process.env.SERVER_ADDRESS || "http://localhost:8001";
 
 const instance = axios.create({
   baseURL: BASE_URL,
