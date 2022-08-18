dc935e9acc8c2195ba77097f3ffd3b3aab34010e
Bazil Korath <korathbasil@live.com>
2022-08-18 17:16:44 +0530

    feat [client]: add ass media query helper

--- client/src/components/checkout-steps/checkout-payments.tsx
+++ client/src/components/checkout-steps/checkout-payments.tsx
@@ -9,6 +9,43 @@ export const CheckoutPayments = () => {
         </div>
         <h3>PAYMENT METHODE</h3>
       </div>
+      <div className={styles.body}>
+        <p>Please select a payment methode</p>
+
+        <form>
+          <div>
+            <input
+              type="radio"
+              name="paymentMethode"
+              value="DEBIT_CARD"
+              id="debit-card"
+            />
+            <label htmlFor="debit-card">DEBIT CARD</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="paymentMethode"
+              value="CREDIT_CARD"
+              id="credit-card"
+            />
+            <label htmlFor="credit-card">CREDIT CARD</label>
+          </div>
+          <div>
+            <input type="radio" name="paymentMethode" value="UPI" id="upi" />
+            <label htmlFor="upi">UPI</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="paymentMethode"
+              value="CASH_ON_DELIVERY"
+              id="cash-on-delivery"
+            />
+            <label htmlFor="cash-on-delivery">Cash on Delivery</label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
