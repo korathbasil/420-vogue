a307ec86931a4340f94e62a6150e3bb8fb69c059
Bazil Korath <korathbasil@live.com>
2022-08-19 18:30:18 +0530

    feat [client]: design greetings comp

--- client/src/components/greetings/greentings.tsx
+++ client/src/components/greetings/greentings.tsx
@@ -1,9 +1,25 @@
+import Link from "next/link";
+
+import { LogoText } from "components/logo-text/logo-text";
 import styles from "./greentings.module.scss";
 
 export const Greetings = () => {
   return (
-    <div className={styles.greetings}>
-      <h3>Welcome</h3>
+    <div className="container">
+      <div className={styles.greetings}>
+        <div>
+          <h4>Hey,</h4>
+          <div>
+            <h4>Welcome to</h4>
+            <LogoText fontSize="18px" />
+          </div>
+        </div>
+        <div>
+          <Link href="/categories">
+            <button>Go to Categories</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
