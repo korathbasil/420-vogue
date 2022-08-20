534e42bdc527e1f45b3b90652322bc0a15dd4b0a
Bazil Korath <korathbasil@live.com>
2022-08-20 18:15:22 +0530

    chore [server]: add passport and passport-jwt as deps

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 
 import { UsersModule } from 'src/users/users.module';
 import { ProductsModule } from '../products/products.module';
@@ -7,6 +9,11 @@ import { ProductsModule } from '../products/products.module';
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/ecommerce'),
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
     UsersModule,
     ProductsModule,
   ],
--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -7,11 +7,15 @@ import {
   ValidationPipe,
   BadRequestException,
 } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService, CreateUserDto } from 'common-server';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Get()
   getAllUsers() {
@@ -21,11 +25,20 @@ export class UsersController {
   @Post()
   @UsePipes(ValidationPipe)
   async createUser(@Body() userData: CreateUserDto) {
-    const user = await this.usersService.createUser(userData);
-    console.log(23123);
+    try {
+      const user = await this.usersService.createUser(userData);
+      if (!user) return new BadRequestException('User already exists');
 
-    if (!user) return new BadRequestException('User already exists');
+      const token = await this.jwtService.signAsync({
+        _id: user._id,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        email: user.email,
+      });
 
-    return user;
+      return { ...user, token };
+    } catch (e) {
+      return e;
+    }
   }
 }
