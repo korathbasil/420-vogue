98bc9d665bad1bb6d52e684246ff59ef80dec681
Bazil Korath <korathbasil@live.com>
2022-08-24 13:39:20 +0530

    fix [client]: auth query in _app.tsx

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,5 +1,9 @@
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "../styles/globals.css";
@@ -7,11 +11,24 @@ import { axios } from "utils";
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
+function Auth({ Component, ...pageProps }: AppProps) {
   const AnyComponent = Component as any;
+
+  const { data: user, error } = useQuery(["user"], async function () {
+    const { data } = await axios.get("/auth/current-user");
+    if (!data) throw new Error("Not authorozed");
+
+    return data;
+  });
+
+  return <AnyComponent {...pageProps} />;
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <AnyComponent {...pageProps} />
+      {/* <AnyComponent {...pageProps} /> */}
+      <Auth Component={Component} {...pageProps} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
