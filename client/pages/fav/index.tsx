ece1dbc5b65eeb5f85a3e22d55971da259145bc7
Bazil Korath <korathbasil@live.com>
2022-08-27 16:04:40 +0530

    fix [client]: renbame wishlist page into favourites page

---
+++ client/pages/fav/index.tsx
@@ -0,0 +1,13 @@
+import { CHeader } from "components";
+import { ShoppingBag } from "assets/icons";
+
+const FavouritesPage = () => {
+  return (
+    <section>
+      <CHeader />
+      <div className="container">MY FAv</div>
+    </section>
+  );
+};
+
+export default FavouritesPage;
