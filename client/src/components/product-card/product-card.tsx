cb17fbbc9099e0dbf7753e4112dad0f11897f0b2
Bazil Korath <korathbasil@live.com>
2022-08-27 16:12:40 +0530

    fix [client]: change product route => /products/[id]

--- client/src/components/product-card/product-card.tsx
+++ client/src/components/product-card/product-card.tsx
@@ -4,7 +4,7 @@ import styles from "./product-card.module.scss";
 
 export const ProductCard = () => {
   return (
-    <Link href={"/product"}>
+    <Link href={"/products/62ddacacb44fe86c3753e11e"}>
       <div className={styles.card}>
         <img
           src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=80"
