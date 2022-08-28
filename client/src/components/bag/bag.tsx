a4b74495f68b953a34768335c2b9009c0ceed7d5
Bazil Korath <korathbasil@live.com>
2022-08-28 11:59:42 +0530

    feat [client]: add counter to bag product componenet

--- client/src/components/bag/bag.tsx
+++ client/src/components/bag/bag.tsx
@@ -1,7 +1,6 @@
-import { AddBox, MinusBox } from "assets/icons";
-import { FC } from "react";
-
+import { Bolt } from "assets/icons";
 import styles from "./bag.module.scss";
+import { Product } from "./product";
 
 export const Bag = () => {
   return (
@@ -13,47 +12,15 @@ export const Bag = () => {
           <Product />
         </div>
       </div>
-      <div className={styles.summary}></div>
-    </div>
-  );
-};
+      <div className={styles.summary}>
+        <h3>
+          ₹7899 <span>(2 ITEMS)</span>
+        </h3>
 
-interface ProductProps {
-  _id: string;
-  brandname: string;
-  stylename: string;
-  price: number;
-}
-
-const Product: FC = () => {
-  return (
-    <div className={styles.product}>
-      <div className={styles.left}>
-        <img
-          src="https://images.unsplash.com/photo-1549298916-b41d501d3772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2hvZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          alt=""
-        />
-
-        <div className={styles.data}>
-          <p>Nike</p>
-          <h4>Air Jordan</h4>
-          <h3>₹7899</h3>
-          <p>Yellow Cream, 44EU</p>
+        <div className={styles.button}>
+          <Bolt size="20px" fill="var(--clr-white)" />
+          <p>CHECKOUT</p>
         </div>
-      </div>
-      <div className={styles.right}>
-        <div className={styles.actions}>
-          <div className={styles.minus}>
-            <MinusBox size="25px" />
-          </div>
-          <div className={styles.qty}>
-            <p>12</p>
-          </div>
-          <div className={styles.add}>
-            <AddBox size="25px" />
-          </div>
-        </div>
-        <p>Quantity</p>
       </div>
     </div>
   );
