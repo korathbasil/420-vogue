378f3d9153e8e35f435719067a4ab0c0d2b7d1a2
Bazil Korath <korathbasil@live.com>
2022-08-31 13:47:35 +0530

    feat [admin-server]: add admin repo

---
+++ admin-server/src/admin/admin.repository.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, FilterQuery, Types, UpdateQuery } from 'mongoose';
+
+import { AdminDocument, Admin } from './admin.model';
+
+@Injectable()
+export class AdminRepository {
+  constructor(
+    @InjectModel('Admin') private readonly adminModel: Model<AdminDocument>,
+  ) {}
+
+  find(query?: FilterQuery<Admin>) {
+    return this.adminModel.find(query);
+  }
+
+  findOne(query: FilterQuery<Admin>) {
+    return this.adminModel.findOne(query);
+  }
+
+  findById(id: string) {
+    return this.adminModel.findById(id).exec();
+  }
+
+  async insertOne(admin: Admin) {
+    const newAdmin = new this.adminModel(admin);
+    const res = await newAdmin.save();
+    return res;
+  }
+
+  updateOne(id: string, query: UpdateQuery<Admin>) {
+    return this.adminModel.updateOne({ _id: new Types.ObjectId(id) }, query);
+  }
+}
