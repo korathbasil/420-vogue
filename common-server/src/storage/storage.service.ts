33c4f3a1425c0d48e1ce60fec4af68d3b58a7160
Bazil Korath <korathbasil@live.com>
2022-08-31 13:22:27 +0530

    feat [common-server]: add getSignedUrl stoorage service

---
+++ common-server/src/storage/storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from "@nestjs/common";
+import { S3 } from "aws-sdk";
+
+@Injectable()
+export class StorageService {
+  private s3: S3;
+  private readonly _BUCKET_NAME = "static.420vogue.in";
+  private readonly _EXPIRY = 300; // in seconds
+  private readonly config = {
+    Bucket: this._BUCKET_NAME,
+    Expires: this._EXPIRY,
+  };
+
+  constructor() {
+    this.s3 = new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+  }
+
+  getSignedUrls() {
+    return this.s3.getSignedUrlPromise("putObject", {
+      ...this.config,
+      Key: "hello",
+    });
+  }
+}
