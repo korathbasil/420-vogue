2bf3adcbde9fb21f3c6a7480df2fe4ac578b9059
Bazil Korath <korathbasil@live.com>
2022-08-31 15:29:55 +0530

    feat [admin-server]: add token service methodes

--- admin-server/src/auth-token/auth-token.service.ts
+++ admin-server/src/auth-token/auth-token.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+type JWtPayload = {
+  _id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+};
 
 @Injectable()
-export class AuthTokenService {}
+export class AuthTokenService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  sign(payload: JWtPayload) {
+    return this.jwtService.signAsync(payload);
+  }
+
+  verify(token: string) {
+    return this.jwtService.verifyAsync(token, {
+      secret: 'secret',
+    });
+  }
+}
