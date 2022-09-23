3d98bfd0651ca6784bd3686ae7157af752ca8ebb
Bazil Korath <korathbasil@live.com>
2022-09-23 14:05:54 +0530

    feat [admin-server]: add prod config for bootstrap fn

--- admin-server/src/main.ts
+++ admin-server/src/main.ts
@@ -6,12 +6,15 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const PORT: number = parseInt(process.env.PORT) | 8001;
+
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin:
+      process.env.NODE_ENV === 'prod' ? 'mpanel.420vogue.in' : 'localhost:3000',
   });
 
   app.use(cookieParser());
-  await app.listen(8001);
+  await app.listen(PORT);
 }
 bootstrap();
