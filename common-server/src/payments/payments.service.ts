a2b1c9ecae6b62b09a95837aff41e85eabd2b845
Bazil Korath <korathbasil@live.com>
2022-09-25 16:59:05 +0530

    feat [common-service]: add razorpay module

--- common-server/src/payments/payments.service.ts
+++ common-server/src/payments/payments.service.ts
@@ -1,33 +1,17 @@
 import { Injectable } from "@nestjs/common";
-import * as Razorpay from "razorpay";
+
+import { RazorpayService } from "src/razorpay/razorpay.service";
 
 import { PaymentsRepository } from "./payments.repository";
 
 @Injectable()
 export class PaymentsService {
-  instance: any;
-  constructor(private readonly repo: PaymentsRepository) {
-    this.instance = new Razorpay({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_KEY_SECRET,
-    });
-  }
+  constructor(
+    private readonly razorpayService: RazorpayService,
+    private readonly repo: PaymentsRepository
+  ) {}
 
-  async createOrder(amount: number) {
-    const CURRENCY = "INR";
-
-    const order = await this.instance.orders.create({
-      amount,
-      currency: CURRENCY,
-      receipt: new Date().toISOString(),
-    });
-
-    return {
-      id: order.id,
-      amount: order.amount_due,
-      currency: order.currency,
-      receiptId: order.receipt,
-      status: order.status,
-    };
+  async createOrder() {
+    const razorpayOrder = await this.razorpayService.createOrder(100);
   }
 }
