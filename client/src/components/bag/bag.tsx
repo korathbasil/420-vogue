3d83fd4c7b3b95e5594d320434f7f16f35d16e97
Bazil Korath <korathbasil@live.com>
2022-10-03 16:31:42 +0530

    fix [client]: bag item with sizes

--- client/src/components/bag/bag.tsx
+++ client/src/components/bag/bag.tsx
@@ -7,6 +7,14 @@ export const Bag = () => {
   const user = useUserStore((us) => us.user);
   const vBag = useVBagStore((vbs) => vbs.vBag);
 
+  function calculateTotalAmount() {
+    let amt = 0;
+    vBag.forEach((p) => {
+      amt = amt + p.variant.price;
+    });
+    return amt;
+  }
+
   return (
     <div className={styles.bag}>
       <div className={styles.items}>
@@ -14,14 +22,17 @@ export const Bag = () => {
         <div className={styles.products}>
           {!user &&
             vBag.map((p) => {
-              return <MiniProduct product={p} />;
+              return <MiniProduct key={p._id} product={p} />;
             })}
         </div>
       </div>
       <div className={styles.summary}>
-        <h3>
-          ₹7899 <span>(2 ITEMS)</span>
-        </h3>
+        {!user && (
+          <h3>
+            {calculateTotalAmount()}
+            <span> ({vBag.length} ITEMS)</span>
+          </h3>
+        )}
 
         <div className={styles.button}>
           <Bolt size="20px" fill="var(--clr-white)" />
